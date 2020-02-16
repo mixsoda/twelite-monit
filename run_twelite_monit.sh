5a20ab3f3531124b13f8a9cf178f5797572cf2af
mixsoda <39870720+mixsoda@users.noreply.github.com>
2020-02-17 00:23:01 +0900

    Initial commit

---
+++ run_twelite_monit.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+CDIR=`dirname ${0}`
+SHNAME=`basename ${0}`
+cd ${CDIR}/
+
+PY_SCRIPT_NAME='twelite_monitor_logging.py'
+LAST_PID=`cat pid.txt`
+
+RUNNING_FLAG=`ps -x | awk "/${LAST_PID}/{print \\$0}" | awk "/${PY_SCRIPT_NAME}/{print \\$0}" | wc -l`
+
+if [ ${RUNNING_FLAG} -eq 0 ]; then
+    nohup python twelite_monitor_logging.py < /dev/null > /dev/null 2>&1  &
+    echo "Remote window security system started."
+    echo "Remote soil moisture monitoring system started."
+    echo "Process ID="$!
+    echo $! > pid.txt
+fi
