67c522d434926a3478865dab30426d6f2223fdc2
mixsoda <39870720+mixsoda@users.noreply.github.com>
2020-05-26 02:02:52 +0900

    Fixed a bug that doesn't work when run from cron

--- run_twelite_monit.sh
+++ run_twelite_monit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+export PATH="/home/pi/berryconda3/bin:$PATH"
 CDIR=`dirname ${0}`
 SHNAME=`basename ${0}`
 cd ${CDIR}/
@@ -10,7 +11,7 @@ LAST_PID=`cat pid.txt`
 RUNNING_FLAG=`ps -x | awk "/${LAST_PID}/{print \\$0}" | awk "/${PY_SCRIPT_NAME}/{print \\$0}" | wc -l`
 
 if [ ${RUNNING_FLAG} -eq 0 ]; then
-    nohup python twelite_monitor_logging.py < /dev/null > /dev/null 2>&1  &
+    python twelite_monitor_logging.py >/dev/null 2>&1 &
     echo "Remote window security system started."
     echo "Remote soil moisture monitoring system started."
     echo "Process ID="$!
